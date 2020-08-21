7366901be82d90524a5dbb1de326356fecfb8bf8
Harry Chen <i@harrychen.xyz>
2020-08-21 15:49:13 +0800

    Move common dependencies to root project
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     kotlin("jvm") version "1.3.72"
 }
@@ -9,5 +12,34 @@ allprojects {
     repositories {
         mavenCentral()
         jcenter()
+    }
+}
+
+subprojects {
+
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+
+    dependencies {
+        val junitVersion = "5.6.2"
+
+        implementation(kotlin("stdlib-jdk8"))
+        testImplementation("org.junit.jupiter:junit-jupiter-api:${junitVersion}")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${junitVersion}")
+    }
+
+    tasks {
+        compileKotlin {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+        compileTestKotlin {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+        test {
+            useJUnitPlatform()
+            testLogging {
+                exceptionFormat = TestExceptionFormat.FULL
+                events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+            }
+        }
     }
 }
--- http-api/build.gradle.kts
+++ http-api/build.gradle.kts
@@ -1,6 +1,3 @@
-import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-import org.gradle.api.tasks.testing.logging.TestLogEvent
-
 plugins {
     kotlin("jvm")
 }
@@ -8,9 +5,7 @@ plugins {
 dependencies {
     val okhttpVersion = "4.8.1"
     val retrofitVersion = "2.9.0"
-    val junitVersion = "5.6.2"
 
-    implementation(kotlin("stdlib-jdk8"))
     implementation("io.reactivex.rxjava3:rxjava:3.0.5")
     implementation("com.google.code.gson:gson:2.8.6")
     implementation("com.squareup.okhttp3:okhttp:${okhttpVersion}")
@@ -18,24 +13,4 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:${retrofitVersion}")
     implementation("com.squareup.retrofit2:adapter-rxjava3:${retrofitVersion}")
     implementation("com.squareup.retrofit2:converter-gson:${retrofitVersion}")
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:${junitVersion}")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${junitVersion}")
-}
-
-
-tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "1.8"
-    }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "1.8"
-    }
-    test {
-        useJUnitPlatform()
-        testLogging {
-            exceptionFormat = TestExceptionFormat.FULL
-            events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
-        }
-    }
 }
