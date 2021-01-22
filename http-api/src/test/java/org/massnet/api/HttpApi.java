7f45965fdd0c408c08eab6d260dd086762a1c9b2
Harry Chen <i@harrychen.xyz>
2021-01-22 13:12:10 +0800

    Update example Java in http-api
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- http-api/src/test/java/org/massnet/api/HttpApi.java
+++ http-api/src/test/java/org/massnet/api/HttpApi.java
@@ -9,17 +9,30 @@ public class HttpApi {
     public void testHttpApiWithJava() {
         // create API service
         MassNetApiV1 api = MassNetApiV1Impl.getDefaultService();
-        // GET client status
+
+        // GET client status using blocking way
         ClientStatus status = api.getClientStatus().blockingGet();
+        // print in readable format
         System.out.println(status);
-        // send a POST request & print results
+        // convert back to JSON
+        String clientStatusJson = ModelSerializer.getGSON().toJson(status);
+        System.out.println(clientStatusJson);
+
+        // or send a POST request & print results using RxJava
         CreateWalletRequest req = new CreateWalletRequest("12345678", "wallet_1", null);
         System.out.println(req);
         api.createWallet(req)
             .subscribeOn(Schedulers.io())
             .observeOn(Schedulers.computation())
-            .doOnError(System.out::println)
-            .subscribe(System.out::println)
+            .doOnError(e -> {
+                // handle error
+            })
+            .subscribe(result -> {
+                // do something with result
+                System.out.println(result.getWalletId());
+                String createWalletJson = ModelSerializer.getGSON().toJson(result);
+                System.out.println(createWalletJson);
+            })
             .dispose();
     }
 }
