5e265e5c5109b193b87ad354fd02917da1d5777c
Harry Chen <i@harrychen.xyz>
2021-08-12 15:11:53 +0800

    Make something constant
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -1,14 +1,14 @@
 object Constants {
-    val artifactGroup = "org.massnet.sdk"
+    const val artifactGroup = "org.massnet.sdk"
 
-    val pomUrl = "https://github.com/massnetorg/wallet-java-sdk"
-    val pomScmUrl = "https://github.com/massnetorg/wallet-java-sdk"
-    val pomScmConnection = "scm:git:git://github.com/massnetorg/wallet-java-sdk.git"
+    const val pomUrl = "https://github.com/massnetorg/wallet-java-sdk"
+    const val pomScmUrl = "https://github.com/massnetorg/wallet-java-sdk"
+    const val pomScmConnection = "scm:git:git://github.com/massnetorg/wallet-java-sdk.git"
 
-    val pomLicenseName = "MIT"
-    val pomLicenseUrl = "https://opensource.org/licenses/mit-license.php"
-    val pomLicenseDist = "repo"
+    const val pomLicenseName = "MIT"
+    const val pomLicenseUrl = "https://opensource.org/licenses/mit-license.php"
+    const val pomLicenseDist = "repo"
 
-    val pomDeveloperId = "Harry-Chen"
-    val pomDeveloperName = "Shengqi Chen"
+    const val pomDeveloperId = "Harry-Chen"
+    const val pomDeveloperName = "Shengqi Chen"
 }
